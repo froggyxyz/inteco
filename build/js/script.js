ded31f66abaec5068643931fed1128c5695a34f0
froggyxyz <slava.ikonnikov5544@mail.com>
2022-01-20 23:58:30 +0700

    .

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,38 @@
-$('#projects__slider').slick({ arrows: false });
-$('#principles__slider').slick({ arrows: false });
-$('#company__slider').slick({ arrows: false });
+$('#projects__slider').slick({
+  arrows: false,
+  dots: true,
+  dotsClass: 'projects__dots',
+  slidesToScroll: 1,
+  slidesToShow: 1,
+});
+$('#principles__slider').slick({
+  arrows: false,
+  dots: true,
+  dotsClass: 'principles__dots',
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 1339,
+      settings: {
+        dots: false,
+        dotsClass: '',
+      },
+    },
+  ],
+});
+$('#company__slider').slick({
+  arrows: false,
+  dots: true,
+  dotsClass: 'company__dots',
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 1340,
+      settings: {
+        dots: false,
+      },
+    },
+  ],
+});
