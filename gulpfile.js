bfc37ec61d6039f7e5b47e03c4cd2c319a255cc7
froggyxyz <slava.ikonnikov5544@mail.com>
2022-01-21 12:02:42 +0700

    work completed

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,8 @@ let autoprefixer = require('gulp-autoprefixer');
 function buildStyles() {
   return src('./src/scss/style.scss')
     .pipe(sourcemaps.init())
-    .pipe(
-      autoprefixer({
-        overrideBrowserslist: ['last 2 versions'],
-        browsers: [
-          'Android >= 4',
-          'Chrome >= 20',
-          'Firefox >= 24',
-          'Explorer >= 11',
-          'iOS >= 6',
-          'Opera >= 12',
-          'Safari >= 6',
-        ],
-      })
-    )
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(sourcemaps.write())
     .pipe(dest('./build/css'));
 }
